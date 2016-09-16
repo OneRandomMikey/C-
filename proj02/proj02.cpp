e19573997bdc559b59db57f70de6d1ae0eb1408a
Zehua Li <zehua.li6@gmail.com>
2016-09-16 13:59:42 -0400

    Learning C++

--- proj02/proj02.cpp
+++ proj02/proj02.cpp
@@ -11,45 +11,52 @@ using std::cout; using std::endl;using std::cin; using std::fixed;
 #include <iomanip>
 using std::setprecision;
 #include <cmath>
-
+using std::abs;
 
 int main(){
-	double x,y,pi,epsilon,ans,gl_series,n_series,n;
+	double epsilon,new_gl=10,new_n=10,old_gl=0,old_n=0,series_gl,series_n;
+	long first_time_gl=0,first_time_n=0,n_gl=0,count_gl=0,count_n=0,n_n=0;
 	cin>>epsilon;
-	//cin>>x;
-	x=0;
-	y=0;
-	ans=0;
-	n=0;
-	pi=3.1415926535897;
-	while (fabs(pi-gl_series)>epsilon && fabs(pi-n_series)>epsilon){
-		x+=(-pow(1,n))/(2*n+1);
-		gl_series=4*x;
-		
-		y+=(pow(-1,n))/((2*n+2)*(2*n+3)*(2*n+4));
-		n_series=3+4*y;
-		
-		n++;
+	if (epsilon==0){
+		new_gl-=10;
+		new_n-=10;
+		cout<<fixed<<setprecision(12)<<new_gl<<" "<<count_gl<<endl;
+		cout<<fixed<<setprecision(12)<<new_n<<" "<<count_n<<endl;
+	}
+	else{
+		while(abs(new_gl-old_gl)>epsilon){
+			if (first_time_gl>0){
+				old_gl=new_gl;
+				series_gl=(new_gl/4)+((pow(-1,n_gl))/(2*n_gl+1));
+				new_gl=4*series_gl;
+				count_gl++;
+				n_gl++;
+			}
+			else{
+				series_gl=((pow(-1,n_gl))/(2*n_gl+1));
+				n_gl++;
+				new_gl=4*series_gl;
+				count_gl++;
+				first_time_gl++;
+			}
 		}
-	cout<<fixed<<setprecision(15)<<n_series<<endl<<gl_series<<endl<<n<<endl;
-	return 0;
+		cout<<fixed<<setprecision(12)<<new_gl<<" "<<count_gl<<endl;
+		while(abs(new_n-old_n)>epsilon){
+			if (first_time_n>0){
+				old_n=new_n;
+				series_n=((new_n-3)/4)+(pow(-1,n_n))/((2*n_n+2)*(2*n_n+3)*(2*n_n+4));
+				new_n=3+4*series_n;
+				count_n++;
+				n_n++;
+			}
+			else{
+				series_n=(pow(-1,n_n))/((2*n_n+2)*(2*n_n+3)*(2*n_n+4));
+				n_n++;
+				new_n=3+4*series_n;
+				count_n++;
+				first_time_n++;
+			}
+		}
+		cout<<fixed<<setprecision(12)<<new_n<<" "<<count_n<<endl;
+	}
 }
-	//((pi-(3+4*ans)>epsilon)//{
-		//ans+=(pow(-1,x))/((2*x+2)*(2*x+3)*(2*x+4));
-		//x+=1;
-		
-		
-		
-//	cout<<3+4*ans<<endl<<x<<endl;
-	
-	//for (;x<=19;x++){
-		//y+=(pow(-1,x))/((2*x+2)*(2*x+3)*(2*x+4));
-		//}
-	//pi=3+4*y;
-	//cout<<fixed<<setprecision(15)<<pi<<endl;
-	 
-	//cout<<EPSILON;
-	
-
-
-
