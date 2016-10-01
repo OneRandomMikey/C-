d7d3d3aabe129b42c225f16666ec5020af6217cf
OneRandomMikey <zehua.li6@gmail.com>
2016-10-01 18:42:23 -0400

    palindrome

--- proj04/proj04.cpp
+++ proj04/proj04.cpp
@@ -20,11 +20,14 @@ bool is_palindrome(long n){
         char front, back;
         front = converted_n[compare_time - 1];
         back = converted_n[converted_n.length() - compare_time];
+        //cout<<"front: "<< front<<endl;
+       // cout<<"back: "<<back<<endl;
         if (front == back){
             palindrome = true;
         }
         else{
             palindrome = false;
+            break;
         }
     }
     return palindrome;
@@ -58,14 +61,72 @@ bool check_args(long limit , long &first , long &second){
 }
 
 bool check_lychrel(long n, long limit){
+    long iterations = 0, n_rev, run_time=0;
+    bool check = false, check_return;
 
+    while ( check == false & run_time <= (limit+1)){
+        if (is_palindrome(n) == false){
+            n_rev = rev_num (n);
+            n += n_rev;
+            iterations ++;
+            check = false;
+            //cout<< "something n: "<<n<<endl;
+           // cout<< "if iterations: "<<iterations <<endl;
+            run_time ++;
+        }
+        else{
+            check = true;
+           // cout<< " else iterations: "<<iterations <<endl;
+        }
+    }
+    if (iterations > (limit)){
+       // cout<<"something iterations: "<<iterations<<endl;
+        check_return = true;
+    }
+    else{
+        check_return = false;
+    }
+    return check_return;
 }
 
-int main(){
-    long n, limit, first,second;
-    cin>>n>>limit>>first>>second;
-    cout<<boolalpha;
-    cout<<rev_num(n)<<endl;
-    cout<<is_palindrome(n)<<endl;
-    cout<<check_args(limit,first,second)<<endl;
+long check_range(long start, long end, long limit, long &natural_cnt, long &pal_cnt){
+    long count = 0;
+
+    for (start = start ; start <= end ; start ++){
+        if (is_palindrome(start)){
+            natural_cnt ++;
+        }
+        else{
+            if (check_lychrel(start, limit)){
+                count ++;
+
+            }
+            else{
+
+                pal_cnt++;
+            }
+        }
+    }
+    return count;
+}
+
+
+int main() {
+    cout << boolalpha;
+    long num, limit;
+    long start, end;
+    long natural_cnt=0, pal_cnt=0, lychrel_cnt = 0;
+
+    cin >> num;
+    cout << rev_num(num)<<endl;
+    cin >> num;
+    cout << is_palindrome(num)<<endl;
+    cin >> num >> limit;
+    cout << check_lychrel(num,limit)<< endl;
+    cin >> start >> end >> limit;
+    if (check_args(limit, start, end)){
+      lychrel_cnt = check_range(start, end, limit, natural_cnt, pal_cnt);
+      cout << lychrel_cnt << " " << natural_cnt << " " << pal_cnt << endl;
+    }
+    else cout << "error" << endl;
 }
